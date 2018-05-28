3fae19dc32a170e91fc7cd9cc184e03aee9966c5
Rodion kovalev <rodion.kovalev@mail.ru>
2018-05-28 22:13:21 +0300

    rodja nice

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,8 +19,10 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/favicon-32x32.png" type="image/x-icon">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    
     <?php $this->head() ?>
 </head>
 <body>
@@ -68,13 +70,13 @@ AppAsset::register($this);
 
 
 
-    <div class="container">
+    <!--<div class="container">-->
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
-    </div>
+    <!--</div>-->
 <!--</div>-->
 
 <footer class="footer">
--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,265 @@
 <?php
-
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'ActiveBox';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+<div class="FEATURES" id="features">
+    <div class="row title">
+        <div class="col-lg-12 col-md-12 text_center">
+            <div class="col-sm-12 text_title">
+                YOUR FAVORITE ONE PAGE <br>MULTI PURPOSE TEMPLATE
+            </div>
+            <div class="col-sm-12 text_title2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna
+                <br>vel scelerisque nisl consectetur et.
+            </div>
+            <div class="button__find">
+                <button type="button" class="btn buttom_red" onclick="this.blur();">Find out more</button>
+            </div>
+        </div>
     </div>
-
-    <div class="body-content">
-
+</div>
+<section class="section">
+    <div class="container">
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-md-4 col-sm-6 features text-center">
+                <span class="icon icon-tools"></span>
+                <h5>Easily Customised</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-md-4 col-sm-6 features text-center">
+                <span class="icon icon-desktop"></span>
+                <h5>Responsive Ready</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-md-4 col-sm-6 features text-center">
+                <span class="icon icon-lightbulb"></span>
+                <h5>Modern Design</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 features text-center">
+                <span class="icon icon icon-genius"></span>
+                <h5>Clean Code</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
+            </div>
+            <div class="col-md-4 col-sm-6  features text-center">
+                <span class="icon icon-briefcase"></span>
+                <h5>Ready to Ship</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
+            </div>
+            <div class="col-md-4 col-sm-6 features text-center">
+                <span class="icon icon-download"></span>
+                <h5>Download for Free</h5>
+                <p>Your Favorite One Page Multi Purpose Template</p>
             </div>
         </div>
 
+    </div>
+</section>
+
+<section id="works" class="section">
+    <div class="container-fluid">
+        <div class="row photo_gallery">
+
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5 class>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 fadeIn animated photo_pad">
+                <a href="img/work-1.jpg" class="work-box" data-fancybox="example_group" data-caption="Заголовок">
+                    <img src="img/work-1.jpg" alt="" class="img_p">
+
+                    <div class="text_photo">
+                        <h5>Project Name</h5>
+                        <p>Website Design</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 author_block" id="author_block">
+                <div class="author_block_title">
+                    <div class="photo_author">
+                        <img src="img/team-1.jpg" alt="photo">
+                    </div>
+                    <div class="author_text">
+                        <h4>Ruth Wood </h4>
+                        <h5>FOUNDER, CEO</h5>
+                        <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                            justo sit amet risus.
+                            Maecenas sed diam eget risus varius blandit sit amet non magna.
+                            Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+                    </div>
+                    <ul class="social-icons clearfix">
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                        <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 author_block">
+                <div class="author_block_title">
+                    <div class="photo_author">
+                        <img src="img/team-1.jpg" alt="photo">
+                    </div>
+                    <div class="author_text">
+                        <h4>Ruth Wood </h4>
+                        <h5>FOUNDER, CEO</h5>
+                        <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                            justo sit amet risus.
+                            Maecenas sed diam eget risus varius blandit sit amet non magna.
+                            Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+                    </div>
+                    <ul class="social-icons clearfix">
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                        <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 author_block">
+                <div class="author_block_title">
+                    <div class="photo_author">
+                        <img src="img/team-1.jpg" alt="photo">
+                    </div>
+                    <div class="author_text">
+                        <h4>Ruth Wood </h4>
+                        <h5>FOUNDER, CEO</h5>
+                        <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                            justo sit amet risus.
+                            Maecenas sed diam eget risus varius blandit sit amet non magna.
+                            Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+                    </div>
+                    <ul class="social-icons clearfix">
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                        <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 author_block">
+                <div class="author_block_title">
+                    <div class="photo_author">
+                        <img src="img/team-1.jpg" alt="photo">
+                    </div>
+                    <div class="author_text">
+                        <h4>Ruth Wood </h4>
+                        <h5>FOUNDER, CEO</h5>
+                        <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                            justo sit amet risus.
+                            Maecenas sed diam eget risus varius blandit sit amet non magna.
+                            Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+                    </div>
+                    <ul class="social-icons clearfix">
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+                        <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    </div>
+
+</section>
+
+<div class="flexslider">
+    <ul class="slides">
+        <li>
+            <img src="img/testimonial-2.jpg"/>
+        </li>
+        <li>
+            <img src="img/testimonial-2.jpg"/>
+        </li>
+    </ul>
+</div>
+<div class="container">
+    <div class="row block_up_footer" id="block_up_footer">
+        <div class="col-md-12  text_center_footer">
+
+            <h4>Are You Ready to Start? Download Now For Free!</h4>
+            <h5>FUSCE DAPIBUS, TELLUS AC CURSUS COMMODO</h5>
+            <button type="button" class="btn buttom_red buttom_red_footer">Download for free</button>
+        </div>
     </div>
 </div>

92437a721ebd9afae226aa8454019173599e7d0a
VS <DeadGod1994@gmail.com>
2018-05-31 22:47:54 +0300

    бла-бла-бла

---
+++ controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DeadG
+ * Date: 5/28/2018
+ * Time: 11:55 PM
+ */
+
+namespace app\controllers;
+
+
+class CategoryController
+{
+
+}
---
+++ views/Category/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DeadG
+ * Date: 5/28/2018
+ * Time: 11:56 PM
+ */

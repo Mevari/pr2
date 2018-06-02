bc518538fae281803f8f4e6e3d3d00e0fd110443
VS <DeadGod1994@gmail.com>
2018-06-02 14:22:45 +0300

    213

--- views/category/index.php
+++ views/category/index.php
@@ -1,33 +1,9 @@
 <?php
-
-use yii\helpers\Html;
-use yii\grid\GridView;
-
-/* @var $this yii\web\View */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Categories';
-$this->params['breadcrumbs'][] = $this->title;
+/**
+ * Created by PhpStorm.
+ * User: DeadG
+ * Date: 5/28/2018
+ * Time: 11:56 PM
+ */
+echo "УРААА";
 ?>
-<div class="category-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'Name',
-            'image',
-            'parent_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-</div>

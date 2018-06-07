a48b9f1f30718162b226a87130234c4a956498ca
VS <DeadGod1994@gmail.com>
2018-06-07 23:01:43 +0300

    111

--- views/Category/index.php
+++ views/Category/index.php
@@ -1,5 +1,7 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\LinkPager;
 
 $this->title = 'Categories';
 $this->params['breadcrumbs'][] = $this->title;
@@ -7,27 +9,42 @@ $this->params['breadcrumbs'][] = $this->title;
 <section>
     <div class="container-fluid">
         <div class="row">
-            <div class="hidden-xs col-sm-2">
+            <div class=" col-sm-2">
                 <? echo app\components\MenuWidget::widget(); ?>
+
             </div>
             <div class="col-lg-10 col-sm-10 ">
-            <?php foreach ($Items as $row): ?>
+                <?php foreach ($Items as $row): ?>
 
-                <div class="col-lg-2 col-sm-2">
-                    <div class="item">
-                        <div class="img_cont">
-                            <?= Html::img('/' . $row['Img'], ['style' => 'width:auto;height:inherit;max-width:100%;']) ?>
+                    <div class="col-lg-2 col-sm-2">
+                        <div class="item_catalog">
+
+                            <a href=<?= URL::to(['article/index', 'id' => $row['id']]); ?>>
+                                <div class="img_cont">
+                                    <?= Html::img('/' . $row['Img']) ?>
+                                </div>
+                            </a>
+
+                            <a href="#">
+                                <p><?= $row['Name'] ?></p>
+                            </a>
+                            <p class="price"><?= $row['Price'] . ' руб' ?></p>
+                            <i class="far fa-address-book"></i>
                         </div>
-                        <a href="#">
-                            <p><?= $row['Name'] ?></p>
-                        </a>
-                        <p><?= $row['Price'] ?></p>
                     </div>
-                </div>
-
                 <?php endforeach; ?>
+
+
             </div>
 
+        </div>
+        <div class="row">
+
+            <div class="col-lg-10 col-lg-offset- col-sm-10  col-sm-offset-2">
+            <?php echo LinkPager::widget([
+                'pagination' => $pages,
+            ]); ?>
+            </div>
         </div>
     </div>
 </section>

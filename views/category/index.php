1363b50aa3db3bb8fbefaf9397af43258b191543
Rodion kovalev <rodion.kovalev@mail.ru>
2018-06-08 19:24:43 +0300

    commit php_storm

--- views/category/index.php
+++ views/category/index.php
@@ -2,7 +2,6 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\LinkPager;
-
 $this->title = 'Categories';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -11,14 +10,11 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="row">
             <div class=" col-sm-2">
                 <? echo app\components\MenuWidget::widget(); ?>
-
             </div>
             <div class="col-lg-10 col-sm-10 ">
                 <?php foreach ($Items as $row): ?>
-
                     <div class="col-lg-3 col-sm-3">
                         <div class="item_catalog">
-
                             <a href=<?= URL::to(['article/index', 'id' => $row['id']]); ?>>
                                 <div class="img_cont">
                                     <?= Html::img('/' . $row['Img']) ?>
@@ -36,13 +32,9 @@ $this->params['breadcrumbs'][] = $this->title;
                         </div>
                     </div>
                 <?php endforeach; ?>
-
-
             </div>
-
         </div>
         <div class="row">
-
             <div class="col-lg-10 col-lg-offset- col-sm-10  col-sm-offset-2">
             <?php echo LinkPager::widget([
                 'pagination' => $pages,
